d9e5dda1bd12d0fe75809c4ff7c4bc563e05ea2b
Maksim <bi4ok32@gmail.com>
2022-06-20 15:19:32 +0300

    исправлена анимация русалки
    изменен выбор цели у замка

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -29,6 +29,8 @@ public class GameHandler : MonoBehaviour
 
     [SerializeField]
     private Text nextWaveText;
+    [SerializeField]
+    private float timeForFirstWave=30f;
 
     [SerializeField]
     private GameObject player;
@@ -62,7 +64,7 @@ public class GameHandler : MonoBehaviour
         scrapStorage = new Dictionary<string, float>() { { "red", 5 }, { "blue", 250 }, { "yellow", 5} };
         UpdateInfo();
         menuScript.ChangeStateToBuild();
-        timeForStartNextWave = Time.time + 30;
+        timeForStartNextWave = Time.time + timeForFirstWave;
         soundmessr.PlayMessege();
     }
 
