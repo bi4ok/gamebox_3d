043e0e871afdd34b3747777a77f8649d1fe4950c
Maksim <bi4ok32@gmail.com>
2022-06-21 11:23:08 +0300

    баги с мобами исправлены (щит снимается сразу, мобы близко к базе не подходят, издалека базу не бьют)

--- Assets/Scripts/CastleController.cs
+++ Assets/Scripts/CastleController.cs
@@ -64,7 +64,6 @@ public class CastleController : MonoBehaviour, IDamageAble
     {
         if (_alive)
         {
-            print(_alive + " alive");
             _stats.TakeDamage(damageAmount, damageFrom);
             DiedByDamage();
             
